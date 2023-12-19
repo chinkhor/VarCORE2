6d184c34ea7bfe602d01cb103d869536225a474e
chinkhor <chin.kooi.khor@gmail.com>
2023-12-18 19:23:31 -0600

    Force auto accept in sudo apt install

--- setup_env.sh
+++ setup_env.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install openjdk-11-jdk-headless:amd64
-sudo apt install make
-sudo apt install cmake
-sudo apt install gcc
-sudo apt install swig
-sudo apt install libreadline-dev
-sudo apt install libperl-dev
-sudo apt install unzip
+sudo apt-get update
+sudo apt-get --yes --force-yes install python3
+sudo apt-get --yes --force-yes install python3-pip
+sudo apt-get --yes --force-yes install openjdk-11-jdk-headless
+sudo apt-get --yes --force-yes install make
+sudo apt-get --yes --force-yes install cmake
+sudo apt-get --yes --force-yes install gcc
+sudo apt-get --yes --force-yes install swig
+sudo apt-get --yes --force-yes install libreadline-dev
+sudo apt-get --yes --force-yes install libperl-dev
+sudo apt-get --yes --force-yes install unzip
 
