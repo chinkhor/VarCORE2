56eb3f32ca807fba34c815c1df74820f7b0c517d
chinkhor <chin.kooi.khor@gmail.com>
2024-03-26 21:56:39 -0500

    Update script to use gdown to download google drive

--- download_acts.sh
+++ download_acts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O ACTS3.2.zip "https://drive.google.com/uc?id=1TERK99sNwSrKaUbHfvjojogfdjxqP7cp&export=download&confirm=yes"
+gdown "https://drive.google.com/uc?id=1TERK99sNwSrKaUbHfvjojogfdjxqP7cp&export=download&confirm=yes"
 unzip ACTS3.2.zip
 rm ACTS3.2.zip
 mv ACTS3.2/acts_3.2.jar .
--- setup_env.sh
+++ setup_env.sh
@@ -10,4 +10,4 @@ sudo apt-get --yes --force-yes install swig
 sudo apt-get --yes --force-yes install libreadline-dev
 sudo apt-get --yes --force-yes install libperl-dev
 sudo apt-get --yes --force-yes install unzip
-
+pip install gdown
